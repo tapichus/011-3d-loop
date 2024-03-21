6cb19c068c9aa12d542c968266c468186aeed209
Gerardo Cara <81977609+tapichus@users.noreply.github.com>
2024-03-21 16:12:53 -0300

    Add files via upload

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
 import styles from './page.module.css'
 import dynamic from 'next/dynamic'
 
-const Scene = dynamic(() => import('@/Components/Scene'), {
+const Scene = dynamic(() => import('../components/Scene'), { 
     ssr: false,
 })
-
 export default function Home() {
   return (
     <main className={styles.main}>

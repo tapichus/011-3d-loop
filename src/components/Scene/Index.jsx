1acbee560581de7bf669ace01e00ccea8d7e6c7b
tapichus <ceroonce@gmail.com>
2024-03-21 11:12:16 -0300

    agregando archivos

---
+++ src/components/Scene/Index.jsx
@@ -0,0 +1,16 @@
+'use client';
+import { Canvas } from '@react-three/fiber'
+import Model from './Model';
+import { Environment } from '@react-three/drei'
+
+export default function Index() {
+  return (
+    <Canvas style={{background: '#000000'}}>
+        <Model />
+        <directionalLight intensity={2} position={[0, 2, 5]}/>
+        <Environment preset="city" />
+    </Canvas>
+  )
+}
+
+
---
+++ src/components/Scene/Model.js
@@ -0,0 +1,35 @@
+import React, { useRef } from 'react'
+import { MeshTransmissionMaterial, useGLTF, Text } from "@react-three/drei"
+import { useFrame, useThree } from '@react-three/fiber'
+import { useControls } from 'leva'
+
+export default function Model() {
+    const { nodes } = useGLTF("/medias/011.glb");
+    const { viewport } = useThree()
+    const torus = useRef(null);
+    
+    useFrame( () => {
+        torus.current.rotation.x += 0.008
+    })
+
+    const materialProps = useControls({
+        thickness: { value: 0.2 },
+        roughness: { step: 0.1 },
+        transmission: {value: 1},
+        ior: { value: 1.2 },
+        chromaticAberration: { value: 0.6},
+        backside: { value: false},
+    })
+    
+    return (
+        <group scale={viewport.width / 3.75} >
+            <Text font={'/fonts/PPNeueMontreal-Bold.otf'} position={[0, 0, -1]} fontSize={0.5} color="white" anchorX="center" anchorY="middle">
+                ceroonce
+            </Text>
+
+            <mesh ref={torus} {...nodes.Curve}>
+                <MeshTransmissionMaterial {...materialProps}/>
+            </mesh>
+        </group>
+    )
+}
